1cc880e721221367f21a6cda633b5de969a1509a
Misbahul Haque <misbahulhaque2001@gmail.com>
2023-04-12 01:37:07 +0530

    Initial Commit

---
+++ PersonalDiaryBackend/src/main/java/com/collegegroup/personaldiary/PersonalDiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.collegegroup.personaldiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDiaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDiaryBackendApplication.class, args);
+	}
+
+}
---
+++ PersonalDiaryBackend/src/test/java/com/collegegroup/personaldiary/PersonalDiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegegroup.personaldiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDiaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
